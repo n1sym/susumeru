143145c2ea3cd5212fba109f4a14c57c92251864
EC2 Default User <ec2-user@ip-172-31-46-97.us-east-2.compute.internal>
2019-06-06 10:23:28 +0000

    add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,9 +19,9 @@ class UsersController < ApplicationController
    def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "IDを登録しました"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "アカウントを有効化してください"
+      redirect_to root_url
     else
       render 'new'
     end

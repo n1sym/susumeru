0704fdd23f77470acc99fbd1076632ff7b82b55b
EC2 Default User <ec2-user@ip-172-31-46-97.us-east-2.compute.internal>
2019-06-06 12:56:56 +0000

    add password reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190606083535) do
+ActiveRecord::Schema.define(version: 20190606115933) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,10 +19,12 @@ ActiveRecord::Schema.define(version: 20190606083535) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin"
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
-    t.boolean "admin"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
